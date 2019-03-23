74bd5ce2f00aaceda376a8db8e515f4699658ad0
John <1101648204@qq.com>
2019-03-23 16:05:02 +0800

    ①测试方法

---
+++ src/main/java/com/saas/adapter/tools/test/TestController.java
@@ -0,0 +1,10 @@
+package com.saas.adapter.tools.test;
+
+/**
+ * Created by Mr.John on 2019/3/23 16:03.
+ **/
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println("测试方法！");
+    }
+}
